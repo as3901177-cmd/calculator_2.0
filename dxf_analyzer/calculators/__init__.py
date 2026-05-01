cb3f494ecca011cbe938b0692c99fbd10d50c7b8
as3901177-cmd <as3901177@gmail.com>
2026-05-01 13:30:13 +0300

    Update __init__.py

--- dxf_analyzer/calculators/__init__.py
+++ dxf_analyzer/calculators/__init__.py
@@ -1,5 +1,6 @@
 """Length calculators for DXF entities"""
 
 from .registry import get_calculator, register_calculator
+from .overlap_handler import OverlapHandler
 
-__all__ = ['get_calculator', 'register_calculator']
+__all__ = ['get_calculator', 'register_calculator', 'OverlapHandler']
