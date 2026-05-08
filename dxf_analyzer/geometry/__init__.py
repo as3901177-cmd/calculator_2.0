caf0fa9cfd6a376560b83bc600f4bc5b188e55f7
as3901177-cmd <as3901177@gmail.com>
2026-05-08 13:40:51 +0300

    Update __init__.py

--- dxf_analyzer/geometry/__init__.py
+++ dxf_analyzer/geometry/__init__.py
@@ -1,12 +1,19 @@
 """Geometry operations module"""
 
-from .transforms import get_entity_center, check_is_closed, get_endpoints
+from .transforms import (
+    get_entity_center,
+    check_is_closed,
+    validate_closure,
+    get_endpoints,
+    distance_between_points
+)
 from .connectivity import build_connectivity_graph, find_connected_chains
 from .piercing_counter import count_piercings_advanced
 from .chain_builder import assign_chain_ids
 
 __all__ = [
-    'get_entity_center', 'check_is_closed', 'get_endpoints',
+    'get_entity_center', 'check_is_closed', 'validate_closure',
+    'get_endpoints', 'distance_between_points',
     'build_connectivity_graph', 'find_connected_chains',
     'count_piercings_advanced', 'assign_chain_ids'
 ]
